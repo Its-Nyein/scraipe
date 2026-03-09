0407f10e4b5126b824801ef5d3d972405fc3b884
nyein phyo aung <146245393+Its-Nyein@users.noreply.github.com>
2026-03-09 23:28:58 +0630

    chore: extracting structured data and stored in db

--- src/lib/scrape.ts
+++ src/lib/scrape.ts
@@ -1,14 +1,64 @@
-import { importSchema } from "@/schemas/import";
+import { prisma } from "#/db";
+import {
+  importSchema,
+  scrapeExtractSchema,
+  type ScrapeExtractSchema,
+} from "@/schemas/import";
 import { createServerFn } from "@tanstack/react-start";
 import { firecrawl } from "./fireclaw";
+import { getSession } from "./session";
 
 export const getItems = createServerFn({ method: "POST" })
   .inputValidator(importSchema)
   .handler(async ({ data }) => {
-    const result = await firecrawl.scrape(data.url, {
-      formats: ["markdown"],
-      onlyMainContent: true,
+    const user = await getSession();
+    if (!user?.user) {
+      throw new Error("Unauthorized");
+    }
+
+    const scrapedData = await prisma.scrapedData.create({
+      data: {
+        url: data.url,
+        userId: user.user.id,
+        status: "PROCESSING",
+      },
     });
 
-    console.log(result);
+    try {
+      const result = await firecrawl.scrape(data.url, {
+        formats: [
+          "markdown",
+          {
+            type: "json",
+            schema: scrapeExtractSchema,
+          },
+        ],
+        onlyMainContent: true,
+      });
+
+      const jsonData = result.json as ScrapeExtractSchema;
+
+      const updatedScrapedData = await prisma.scrapedData.update({
+        where: { id: scrapedData.id },
+        data: {
+          title: result.metadata?.title,
+          content: result.markdown,
+          ogImage: result.metadata?.ogImage,
+          author: jsonData.author ?? null,
+          publishedAt: new Date(jsonData.publishedAt ?? ""),
+          status: "COMPLETED",
+        },
+      });
+
+      return updatedScrapedData;
+    } catch (error) {
+      const failedScrapedData = await prisma.scrapedData.update({
+        where: { id: scrapedData.id },
+        data: {
+          status: "FAILED",
+        },
+      });
+
+      return failedScrapedData;
+    }
   });
--- src/schemas/import.ts
+++ src/schemas/import.ts
@@ -1,13 +1,19 @@
 import { z } from "zod";
 
 export const importSchema = z.object({
-  url: z.url(),
+  url: z.string().url(),
 });
 
 export const bulkImportSchema = z.object({
-  urls: z.url(),
+  urls: z.string().url(),
   search: z.string(),
+});
+
+export const scrapeExtractSchema = z.object({
+  author: z.string().nullable(),
+  publishedAt: z.string().nullable(),
 });
 
 export type ImportSchema = z.infer<typeof importSchema>;
 export type BulkImportSchema = z.infer<typeof bulkImportSchema>;
+export type ScrapeExtractSchema = z.infer<typeof scrapeExtractSchema>;
